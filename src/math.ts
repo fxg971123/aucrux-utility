a82c70334cabfd05c25369335b7a344aac471e23
fxg971123 <43235817@qq.com>
2022-11-28 18:28:18 +0800

    test actions

--- src/math.ts
+++ src/math.ts
@@ -3,6 +3,15 @@
  * @param {number} data
  * @return {number}
  */
- export function sum(data: number[]): number {
+export function sum(data: number[]): number {
     return data.reduce((sum, item) => sum + item, 0);
+}
+
+/**
+ * @description: 
+ * @param {number} data
+ * @return {number}
+ */
+export function getAve(data: number[]): number {
+    return sum(data)/data.length;
 }
